e231aa6378d097b7374558d32733f4151bcaad5e
chrisvibeofficial <jueffizzy@gmail.com>
2025-03-11 17:47:19 +0100

    updated

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const { validateRegister } = require('../middlewares/validator');
 
 const router = require('express').Router();
 
-router.post('register', validateRegister, register);
+router.post('/register', validateRegister, register);
 router.get('verify/user', verify);
 
 module.exports = router
