4c8d31b3fc54ccea128b7a93a691bc22ebee8326
chrisvibeofficial <jueffizzy@gmail.com>
2025-03-11 17:29:01 +0100

    verify user

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,4 +57,82 @@ exports.register = async (req, res) => {
       message: 'Error Registering User'
     })
   }
+};
+
+
+exports.verify = async (req, res) => {
+  try {
+    const { token } = req.params;
+
+    if (!token) {
+      return res.status(404).json({
+        message: 'Token not found'
+      })
+    };
+
+    jwt.verify(token, process.env.JWT_SECRET, async (error, payload) => {
+      if (error) {
+        if (error instanceof jwt.JsonWebTokenError) {
+          const { userId } = jwt.decode(token);
+          const user = await userModel.findById(userId);
+
+          if (!user) {
+            return res.status(404).json({
+              message: 'Account not found'
+            })
+          };
+
+          if (user.isVerified === true) {
+            return res.status(400).json({
+              message: 'Account is already verified'
+            })
+          };
+
+          const newToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '5mins' });
+          const link = `${req.protocol}://${req.get('host')}/api/v1/verify/user/${newToken}`;
+
+          const mailFormat = {
+            email: user.email,
+            html: verify(link, user.userName),
+            subject: 'RESEND: ACCOUNT VERIFICATION'
+          };
+      
+          await send_email(mailFormat);
+          res.status(201).json({
+            message: 'Session expired, Link has been sent to email address',
+          })
+        }
+      } else {
+        const user = await userModel.findById(payload.userId);
+
+        if (!user) {
+          return res.status(404).json({
+            message: 'Account not found'
+          })
+        };
+
+        if (user.isVerified === true) {
+          return res.status(400).json({
+            message: 'Account is already verified'
+          })
+        };
+
+        user.isVerified = true;
+        await user.save();
+        res.status(200).json({
+          message: 'Account verified successfully'
+        })
+      }
+    })
+  } catch (error) {
+    console.log(error.message);
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res.status(400).json({
+        message: 'Session expired, Link has been sent to email address'
+      })
+    }
+    res.status(500).json({
+      message: 'Error Verifying User'
+    })
+  }
 }
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const { register, verify } = require('../controllers/userController');
+const { validateRegister } = require('../middlewares/validator');
+
+const router = require('express').Router();
+
+router.post('register', validateRegister, register);
+router.get('verify/user', verify);
+
+module.exports = router
