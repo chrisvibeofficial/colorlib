060a1066711d0eafa0d337305fd5231ef735debb
chrisvibeofficial <jueffizzy@gmail.com>
2025-03-11 21:52:55 +0100

    login

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
-const { register, verify } = require('../controllers/userController');
-const { validateRegister } = require('../middlewares/validator');
+const { register, verify, login } = require('../controllers/userController');
+const { validateRegister, validateLogin } = require('../middlewares/validator');
 
 const router = require('express').Router();
 
 router.post('/register', validateRegister, register);
 router.get('/verify/user/:token', verify);
+router.post('/login', validateLogin, login)
 
 module.exports = router
